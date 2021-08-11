736fd1e58ee27bb485d51976ab561c24e1aae9af
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-11 11:22:02 +0000

    fix typo

--- port-scanner.py
+++ port-scanner.py
@@ -3,13 +3,12 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
 
 host = input("Please enter an IP address: ")
-port = int(input("Please enter a port: "))
+port = str(input("Please enter a port: "))
 
 def portScanner(port):
-    if s.connect_ex((host, port)):
+    if s.connect_ex((host, int(port))):
         print("The port is closed.")
     else:
         print("The port is open.")
